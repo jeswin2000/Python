ae824f04af9372b72183f857af022c3edf24a699
jeswin2000 <97499832+jeswin2000@users.noreply.github.com>
2022-03-06 23:46:55 +0530

    Add files via upload

---
+++ Lambda function to find area.py
@@ -0,0 +1,6 @@
+t_area = lambda p,q,r: p+q+r
+r_area = lambda l,b: l*b
+s_area = lambda side: side*side
+print("Area of triangle(10,20,15) is:",t_area(10,20,15))
+print("Area of rectangle(20,10) is:",r_area(20,10))
+print("Area of square(10) is:",s_area(10))
